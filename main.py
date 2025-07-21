eb7f87946b48c3c29cc52951a10af342f1c28351
Harry Worrell <162141653+HJW5@users.noreply.github.com>
2025-07-21 14:10:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from material import Material
+from degradation_model import plot_degradation
 
-from degradation_model import sim_degradation, simulate_material, plot_degradation
+# Define materials
+pla = Material("PLA", degradation_rate=0.02, youngs_modulus=2500, density=1.25, toxicity_threshold=0.3)
+mg = Material("Magnesium", degradation_rate=0.05, youngs_modulus=45000, density=1.74, toxicity_threshold=0.4)
+
+# Run simulations
+time, mass = pla.simulate()
+plot_degradation(time, mass, material_name=pla.name)
